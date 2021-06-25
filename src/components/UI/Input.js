dee8f37ec99c454ed1cb127218636d4ee77c94e1
JAlejandroMG <jalejandromg@tutanota.com>
2021-06-24 20:22:07 -0500

    Added Shopping cart component

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -6,9 +6,7 @@ const Input = props => {
    return (
       <div className={classes.input}>
          <label htmlFor={props.input.id}>{ props.label }</label>
-         {/* El props.input.id ya esta incluido con el spread operator}*/}
          <input
-            //id={props.input.id} No requiere llevarlo
             { ...props.input }
          />
       </div>
