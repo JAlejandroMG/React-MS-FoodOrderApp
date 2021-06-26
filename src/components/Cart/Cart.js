35b6833e4eceb69e63955b7c23bc5a850f991a52
JAlejandroMG <jalejandromg@tutanota.com>
2021-06-25 22:45:43 -0500

    Added Manage cart and modal state

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,13 +7,13 @@ const Cart = (props) => {
 	const cartItems = (
 		<ul className={classes["cart-items"]}>
 			{[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-				<li>{item.name}</li>
+				<li key={item.id}>{item.name}</li>
 			))}
 		</ul>
 	);
 
 	return (
-		<Modal>
+		<Modal onHideCart={props.onHideCart}>
 		{/* <div> */}
 			{cartItems}
 			<div className={classes.total}>
@@ -21,7 +21,7 @@ const Cart = (props) => {
             <span>$35.62</span>
          </div>
 			<div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
+            <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
             <button className={classes.button}>Order</button>
          </div>
 		{/* </div> */}
