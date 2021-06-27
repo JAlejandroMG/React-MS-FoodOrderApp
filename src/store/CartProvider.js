7caf0eeee80f802eb6ff6a9b4bee49028aa99bad
JAlejandroMG <jalejandromg@tutanota.com>
2021-06-27 14:01:30 -0500

    Added addItemCartHandler in CartProvider.js

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,15 +1,42 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context';
+
+const defaultCartState = {
+   items: [],
+   totalAmount: 0
+};
+
+const cartReducer = (prevState, action) => {
+   if (action.type === 'ADD') {
+      // concat array method returns a new array not affecting prevState (immutability)
+      const updatedItems = state.items.concat(action.item);
+      const updatedTotalAmount = prevState.totalAmount + action.item.amount * action.item.price;
+      return {
+         items: updatedItems,
+         totalAmount: updatedTotalAmount
+      }
+   }
+   return;
+};
 
 
 
 const CartProvider = props => {
-   const addItemToCartHandler = item => {};
+   const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
 
-   const removeItemfromCartHandler = id => {};
+   //* This function is ready, now it needs to be calleds
+   const addItemToCartHandler = item => {
+      dispatchCartAction({type: 'ADD', item: item});
+   };
+
+   const removeItemfromCartHandler = id => {
+      dispatchCartAction({tpe:'REMOVE', id: id})
+   };
 
    const cartContext = {
-      items: [],
-      totalAmount: 0,
+      items: cartState.items,
+      totalAmount: cartState.totalAmount,
       addItem: addItemToCartHandler,
       removeItem: removeItemfromCartHandler
    };
