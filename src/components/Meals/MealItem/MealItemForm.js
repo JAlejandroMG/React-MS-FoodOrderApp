bfb9ebd194a5560d1841931f377ecd8e7de0e836
JAlejandroMG <jalejandromg@tutanota.com>
2021-06-27 14:39:10 -0500

    Added Calling addToCartHandler in MealItem.js

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,20 +1,47 @@
+import { useRef, useState } from 'react';
+
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 
 
 const MealItemForm = props => {
+   const [amountIsValid, setAmountIsValid] = useState(true);
+   //* Cannot be used directly in custom components like <Input />
+   const amountInputRef = useRef();
+
+   const submitHandler = event => {
+      event.preventDefault();
+
+      //* The value from the ref is always a string
+      const enteredAmount = amountInputRef.current.value;
+      const enteredAmountNumber = +enteredAmount;
+
+      if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+         setAmountIsValid(false);
+         return;
+      };
+
+      //* Here I just pass the entered amount, I still need id, name and price of the meal
+      props.onAddToCart(enteredAmountNumber);
+   };
+
    return (
-      <form className={classes.form}>
-         <Input label="Amount" input={{
-            id: props.id,
-            type: 'number',
-            min: '1',
-            max: '5',
-            step: '1',
-            defaultValue: '0'
-         }} />
+      <form className={classes.form} onSubmit={submitHandler}>
+         <Input
+            ref={amountInputRef}
+            label="Amount"
+            input={{
+               id: props.id,
+               type: 'number',
+               min: '1',
+               max: '5',
+               step: '1',
+               defaultValue: '0'
+            }}
+         />
          <button>+ Add</button>
+         {!amountIsValid &&  <p>Please enter a vallid amount (1-5).</p>}
       </form>
    );
 };
